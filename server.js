cd725389ea5764667550bac669609a6c5eb8218c
Kevin Peguero <kevinpeguero2@gmail.com>
2020-05-04 21:52:09 -0600

    made changes to server.js

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ var mysql = require("mysql");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
+var connection ;
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Moonlight21@",
+        database: "Top-trackDB"
+    })
+}
+connection.connect();
+module.exports = connection;
 
 var db = require("./models");
 // Static directory to be served
